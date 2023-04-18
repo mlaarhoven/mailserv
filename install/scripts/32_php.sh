289622e020b9b393cb0b2298e1c9218d1ae838fe
mlaarhoven <mlaarhoven@users.noreply.github.com>
2023-04-18 14:00:22 +0200

    PHP

--- install/scripts/32_php.sh
+++ install/scripts/32_php.sh
@@ -9,7 +9,7 @@ pkg_add -v -m -I \
     php-intl-8.1.16 \
     php-mysqli-8.1.16 \
     php-pdo_mysql-8.1.16 \
-    php-gd-8.1.16 \
+    php-gd-8.1.16
 
 # info
 # /usr/local/share/doc/pkg-readmes/php-8.1
@@ -21,23 +21,13 @@ for i in *; do ln -sf ../php-8.1.sample/$i ../php-8.1/; done
 # Install our local changes to php.ini
 /usr/bin/install -m 644 /var/mailserv/install/templates/php-mailserv.ini /etc/php-8.1/mailserv.ini
 
-# Install php-fpm.conf
-/usr/bin/install -m 644 /var/mailserv/install/templates/php-fpm.conf /etc/ 
+# Change options in php-fpm.conf
+sed -i '/pid = run\/php-fpm.pid/s/^;//' /etc/php-fpm.conf
+sed -i '/pm\.max_requests/s/^;//'       /etc/php-fpm.conf
+#sed -i '/listen.mode/s/0660/0666/'      /etc/php-fpm.conf
 
 # Make php easier to run from CLI
 ln -s /usr/local/bin/php-8.1 /usr/local/bin/php
 
 rcctl enable php81_fpm
 rcctl start  php81_fpm
-
-
-## PHPMYADMIN
-pkg_add -v -m -I phpMyAdmin
-
-# info
-# /usr/local/share/doc/pkg-readmes/phpMyAdmin
-
-sed -i '/cfg/s/cookie/config/g' /var/www/phpMyAdmin/config.inc.php
-sed -i '/cfg/s/localhost/127.0.0.1/g' /var/www/phpMyAdmin/config.inc.php
-sed -i '/AllowNoPassword/s/false/true/g' /var/www/phpMyAdmin/config.inc.php
-
