499200eee395d681144e0bf90e18f5ccbe7b1d49
mlaarhoven <mlaarhoven@users.noreply.github.com>
2024-09-27 18:00:19 +0200

    sqlgrey

--- install/scripts/70_sqlgrey.sh
+++ install/scripts/70_sqlgrey.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 
-if [[ "$1" == "install" ]]; then
+# Only run on install
+[[ "$1" != "install" ]] && exit 1
 
-  pkg_add -v -m sqlgrey
+pkg_add -v -m sqlgrey
 
-  install -m 644 /var/mailserv/install/templates/sqlgrey.conf /etc/sqlgrey/
-  /usr/local/bin/mysqladmin create sqlgrey
-  /usr/local/bin/mysql -e "grant all privileges on sqlgrey.* to 'sqlgrey'@'localhost' identified by 'sqlgrey';"
-  rcctl enable sqlgrey
-  rcctl start  sqlgrey
-  sleep 2
-  /usr/local/bin/mysql sqlgrey -e "alter table connect add id int primary key auto_increment first;"
-  touch /etc/sqlgrey/clients_fqdn_whitelist.local 
-  touch /etc/sqlgrey/clients_ip_whitelist.local 
-fi
+# /usr/local/share/examples/sqlgrey/README
+
+
+# sqlgrey
+# cp -p /usr/local/share/examples/sqlgrey/sqlgrey.conf /etc/sqlgrey/sqlgrey.conf
+# diff /usr/local/share/examples/sqlgrey/sqlgrey.conf /etc/sqlgrey/sqlgrey.conf
+
+## Socket
+#sed -i '/^# inet = 2501/s/^# //'    /etc/sqlgrey/sqlgrey.conf
+
+## Database settings
+sed -i '/^# db_type/s/^# //'        /etc/sqlgrey/sqlgrey.conf
+sed -i '/^db_type/s/ .*$/ = MariaDB/'   /etc/sqlgrey/sqlgrey.conf
+#db_name = sqlgrey
+#db_host = localhost
+#db_port = default
+#db_user = sqlgrey
+sed -i '/^# db_pass/s/^# //'        /etc/sqlgrey/sqlgrey.conf
+sed -i '/^db_pass/s/ .*$/ = sqlgrey/' /etc/sqlgrey/sqlgrey.conf
+
+
+# create databbase and user
+/usr/local/bin/mysqladmin create sqlgrey
+/usr/local/bin/mysql -e "grant all privileges on sqlgrey.* to 'sqlgrey'@'localhost' identified by 'sqlgrey';"
+
+# create files to store whitelists
+touch /etc/sqlgrey/clients_fqdn_whitelist.local 
+touch /etc/sqlgrey/clients_ip_whitelist.local 
+
+rcctl enable sqlgrey
+rcctl start  sqlgrey
+  
+sleep 2
+/usr/local/bin/mysql sqlgrey -e "alter table connect add id int primary key auto_increment first;"
