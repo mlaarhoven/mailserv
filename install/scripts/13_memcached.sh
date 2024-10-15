2b36dc1b72d67771b83928316501f2bc0bfdec7e
mlaarhoven <mlaarhoven@users.noreply.github.com>
2024-10-15 13:14:46 +0200

    /etc/services info

--- install/scripts/13_memcached.sh
+++ install/scripts/13_memcached.sh
@@ -9,6 +9,14 @@ pkg_add -v -m  memcached--
 # info:
 # /usr/local/share/doc/pkg-readmes/memcached
 
+# add missing network services
+if [ `grep -i memcache /etc/services | wc -l` -eq 0 ]; then
+cat <<EOF >> /etc/services
+memcache        11211/tcp       memcached       # Memory cache service
+memcache        11211/udp       memcached       # Memory cache service
+EOF
+fi
+
 rcctl enable memcached
 rcctl set memcached flags `rcctl get memcached flags` --pidfile=/var/run/memcached/memcached.pid
 rcctl start  memcached
